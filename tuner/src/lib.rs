9a19d28025b3ccd6fbbe7a7a95341ff86a2b4dd7
deszczowy <krystian.szklarek@op.pl>
2020-05-14 19:52:17 +0200

    tuner, a config manager

---
+++ tuner/src/lib.rs
@@ -0,0 +1,23 @@
+use serde_json::{Result, Value};
+use std::fs::File;
+use std::path::Path;
+use std::env::var;
+
+pub struct Config {}
+
+pub trait Configurable {
+    fn new(file_name: String) -> Result<Value> ;
+}
+
+impl Configurable for Config {
+    fn new(file_name: String) -> Result<Value> {
+
+        let config_file_name = format!("{}{}", var("HOME").unwrap(), file_name);
+        let config_file_path = Path::new(&config_file_name);
+
+        let json = File::open(config_file_path).expect("config file not found");
+
+        let cfg : Value = serde_json::from_reader(json).expect("error while reading json");
+        Ok(cfg)
+    }
+}
