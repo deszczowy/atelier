fc476e2603e3c9dbe7e182809dfd6c957ddba41e
deszczowy <krystian.szklarek@op.pl>
2020-10-28 18:52:22 +0100

    artist as a service

--- artist/src/main.rs
+++ artist/src/main.rs
@@ -1,32 +1,63 @@
-
 use painting::*;
 
-fn main() {
+use concierge::*;
+use tuner::*;
+use common::log::*;
+use common::serialized::Serialized;
+use common::poke_message::*;
+use common::arguments::*;
+
+fn run(message: String, one_shot: bool) {
     
-    let width = 800 as u32;
-    let height = 350 as u32;
+    let width = 1600 as u32;
+    let height = 700 as u32;
+    let frame = 5 as i32;
+    
+    println!("ARTIST!");
 
-    let choice = 2 as u8;
+    let p : Poke = match serde_json::from_str(&message) {
+        Ok(data) => data,
+        Err(error) => panic!("Unable to read message: {:?}", error),
+    };
 
-    if (choice == 1) {
+    let target = "./".to_string();
+    if !one_shot {
+        let cfg = Config::new("../config/artist.config".to_string()).unwrap();
+        let target = cfg["target"].as_str().unwrap().to_string();
+    }
+
+    if p.action == "CIRCLES" {
         use circles::*;
-
         let mut c = Painting::new(width, height);
         c.initialize();
         c.generate();
-        c.put_a_frame(20, painting::BLACK);
+        c.put_a_frame(frame, painting::BLACK);
         c.save_file("test.png".to_string());
-    }
+    } 
+    else
 
-    if (choice == 2) {
+    if p.action == "STRIPES" {
         use stripes::*;
-
         let mut c = Painting::new(width, height);
         c.initialize();
         c.generate();
-        c.put_a_frame(5, painting::BLACK);
+        c.put_a_frame(frame, painting::BLACK);
         c.save_file("test.png".to_string());
     }
-    
 
+    if one_shot {
+        println!("One shot done!");
+    }
+
+}
+
+fn main() {
+    if is_one_shot() {
+        println!("One shot run!");
+        let message = "{ \"action\":\"STRIPES\"}".to_string();
+        run(message, true);
+    } else {
+        let concierge = Concierge::new();
+        concierge.expect("art".to_string(), &run);
+    }
 }
