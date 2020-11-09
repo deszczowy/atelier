a5d6eec628828cc8c4ab4d33d80b8854ebef09d3
deszczowy <krystian.szklarek@op.pl>
2020-11-09 19:11:10 +0100

    random range for real numbers

--- library/random/src/lib.rs
+++ library/random/src/lib.rs
@@ -8,6 +8,7 @@ pub trait RandomGenerating {
     fn new() -> RandomGenerator;
     fn spit(&mut self, top: i32) -> i32;
     fn spit_range(&mut self, bottom: i32, top : i32) -> i32;
+    fn spit_range_f(&mut self, bottom: f32, top: f32) -> f32;
 }
 
 impl RandomGenerating for RandomGenerator{
@@ -23,5 +24,12 @@ impl RandomGenerating for RandomGenerator{
 
     fn spit_range(&mut self, bottom: i32, top : i32) -> i32 {
         self.generator.gen_range(&bottom, &top) as i32
+    }
+
+    fn spit_range_f(&mut self, bottom: f32, top: f32) -> f32 {
+        self.generator.gen_range(
+            &bottom * 1000f32, 
+            &top * 1000f32
+        ) / 1000f32
     }
 }
