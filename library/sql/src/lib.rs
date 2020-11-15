6cecd442aa2b61cd2e595634271a515466626c51
deszczowy <krystian.szklarek@op.pl>
2020-11-15 19:47:43 +0100

    warning cleanup in sql

--- library/sql/src/lib.rs
+++ library/sql/src/lib.rs
@@ -1,18 +1,7 @@
-use rusqlite::{Connection, Result};
-use rusqlite::NO_PARAMS;
+//use rusqlite::{Connection};
 
 pub struct Database {
-    connection: Connection
 }
 
 pub trait Queryable {
-    fn new(file: String) -> Database;
-}
-
-impl Queryable for Database {
-    fn new(file: String) -> Database {
-        Database {
-            connection: Connection::open(file).unwrap()
-        }
-    }
 }
